ee00305c5a677a7e6e6c28f80605789aab1d94ce
Gaurav J <gauravjayaraj@gmail.com>
2022-06-28 21:51:19 +0530

    fix payload padding issue

--- dump/generateRTP.go
+++ dump/generateRTP.go
@@ -1,43 +1,55 @@
 package main
 
-import(
+import (
 	"fmt"
 	"math/rand"
 	"time"
+
 	"github.com/pion/rtp"
 )
 
-
 func printPkt(pkt rtp.Packet) {
-	fmt.Println("Header :",pkt.Header)
+	fmt.Println("Header :", pkt.Header)
 	fmt.Println("Payload :", pkt.Payload)
-	fmt.Println("PaddingSize :",pkt.PaddingSize, "\n")
+	fmt.Println("PaddingSize :", pkt.PaddingSize, "\n")
 }
 
 func PadPackets(srcBlock *[]rtp.Packet) {
 
-    maxSize := -1
-    n := len(*srcBlock)
+	maxSize := -1
+	n := len(*srcBlock)
 
-    for i := 0; i < n; i++ {
-        currSize := (*srcBlock)[i].MarshalSize()
-        if maxSize < currSize {
-            maxSize = currSize
-        }
-    }
+	for i := 0; i < n; i++ {
+		currSize := len((*srcBlock)[i].Payload)
+		if maxSize < currSize {
+			maxSize = currSize
+		}
+	}
 
-    for i := 0; i < n; i++ {
-    	size := (*srcBlock)[i].MarshalSize()
+	for i := 0; i < n; i++ {
+		size := len((*srcBlock)[i].Payload)
 
-        if size != maxSize {
-            // leaving the length field
-        	leftOverPadBytes := maxSize - size - 1
-        	padding := make([]byte, leftOverPadBytes)
+		if size < maxSize {
+			leftOverPadBytes := maxSize - size
 
-            (*srcBlock)[i].PaddingSize = uint8(leftOverPadBytes + 1)
-            (*srcBlock)[i].Payload = append((*srcBlock)[i].Payload, padding...)
-        }
-    }
+			(*srcBlock)[i].PaddingSize = uint8(leftOverPadBytes)
+
+			payload := make([]byte, maxSize)
+			for id, content := range (*srcBlock)[i].Payload {
+				payload[id] = content
+			}
+
+			fmt.Println(payload)
+
+			(*srcBlock)[i].Payload = payload
+
+			// // leaving the length field
+			// leftOverPadBytes := maxSize - size - 1
+			// padding := make([]byte, leftOverPadBytes)
+
+			// (*srcBlock)[i].Payload = append((*srcBlock)[i].Payload, padding...)
+		}
+	}
 
 }
 
@@ -45,9 +57,9 @@ func GenerateRTP(L int, D int) []rtp.Packet {
 	rand.Seed(time.Now().UnixNano())
 
 	pkts := []byte{
-		0x90, 0xE0, 0x69, 0x8F, 0xD9, 0xC2, 0x93, 0xDA, 
-		0x1C, 0x64, 0x27, 0x82, 0x45, 0xB1, 0x34, 0xF1, 
-		0xFF, 0xFF, 0xAF, 0xFF, 0x98, 0x36, 0xbE, 0x88,
+		0x90, 0xE0, 0x69, 0x8F, 0xD9, 0xC2, 0x93, 0xDA,
+		0x1C, 0x64, 0x27, 0x82, 0x45, 0xB1, 0x34, 0xF1,
+		// 0xFF, 0xFF, 0xAF, 0xFF, 0x98, 0x36, 0xbE, 0x88,
 	}
 
 	size := len(pkts)
@@ -58,20 +70,20 @@ func GenerateRTP(L int, D int) []rtp.Packet {
 
 	packets := []rtp.Packet{}
 
-	for i:=uint16(0); i<n; i++ {
+	for i := uint16(0); i < n; i++ {
 		endIndex := rand.Intn(size)
 
 		packet := rtp.Packet{
 			Header: rtp.Header{
-				Version 		: 2,
-				Padding 		: true,
-				Extension		: false,
-				Marker 			: false,
-				PayloadType 	: 15,
-				SequenceNumber	: SN_base + i,
-				Timestamp		: 54243243,
-				SSRC			: ssrc,
-				CSRC			: []uint32{},
+				Version:        2,
+				Padding:        true,
+				Extension:      false,
+				Marker:         false,
+				PayloadType:    15,
+				SequenceNumber: SN_base + i,
+				Timestamp:      54243243,
+				SSRC:           ssrc,
+				CSRC:           []uint32{},
 			},
 			Payload: pkts[:endIndex],
 		}
@@ -81,10 +93,17 @@ func GenerateRTP(L int, D int) []rtp.Packet {
 	return packets
 }
 
-func main(){
-	packets := GenerateRTP(5,1)
+func main() {
+	packets := GenerateRTP(5, 1)
+
+	fmt.Println("before")
+	for _, packet := range packets {
+		printPkt(packet)
+	}
+
 	PadPackets(&packets)
 
+	fmt.Println("\nafter")
 	for _, packet := range packets {
 		printPkt(packet)
 	}
