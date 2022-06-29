6e584b92243174170a847fc69dc5308e648d7b57
Shreevathsa B K <75425624+ShreevathsaBK@users.noreply.github.com>
2022-06-29 11:02:50 +0530

    pads the RTP packects

---
+++ util/padRTP.go
@@ -0,0 +1,37 @@
+package util
+
+import (
+	"github.com/pion/rtp"
+)
+
+func PadPackets(srcBlock *[]rtp.Packet) {
+	maxSize := -1
+	n := len(*srcBlock)
+
+	for i := 0; i < n; i++ {
+		currSize := len((*srcBlock)[i].Payload)
+		if maxSize < currSize {
+			maxSize = currSize
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		size := len((*srcBlock)[i].Payload)
+
+		if size < maxSize {
+			leftOverPadBytes := maxSize - size
+
+			(*srcBlock)[i].PaddingSize = uint8(leftOverPadBytes + 1)
+
+			// work like immutable entity, so replace with new slice
+			payload := make([]byte, maxSize)
+			copy(payload, (*srcBlock)[i].Payload)
+
+			(*srcBlock)[i].Payload = payload
+		}
+	}
+
+}
+
+
+
