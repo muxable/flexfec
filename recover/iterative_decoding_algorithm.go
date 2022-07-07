0e20f4254e43be409836060d52ef6ab1b61fb958
Shamanth K M <shamanthkolegodu@gmail.com>
2022-07-08 02:04:53 +0530

    Implementation of 2d fec
    sender receiver completed, working on the Iterative Decoding algorithm.

---
+++ recover/iterative_decoding_algorithm.go
@@ -0,0 +1,25 @@
+package recover
+
+import(
+	"fmt"
+	"rtp"
+)
+
+func IterativeDecoding(srcBlock *[]rtp.Packet, repairPacketRows *[]rtp.Packet, repairPacketColumns *[]rtp.Packet)([]rtp.Packet){
+
+	num_recovered_until_this_iteration:=0
+	num_recovered_so_far:=0
+	
+	// Row wise recovery of missing packets
+	// num_recovered_so_far++
+	// can use MissingPacket function for recover_missing_packet.go
+
+	// column wise recovery of missing packets
+	// num_recovered_so_far++
+
+	// if num_recovered_until_this_iteration<num_recovered_so_far
+	// then num_recovered_until_this_iteration=num_recovered_so_far
+	// Reiterate
+
+	// else terminate / return 
+}
